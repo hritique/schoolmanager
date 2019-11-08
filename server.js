63ebae3317d93848220796f3294f7a9a448187c1
hritique <hritiquerungta@gmail.com>
2019-11-08 20:17:35 +0530

    Added stuffs for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -18,6 +19,14 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/students', require('./routes/api/students'));
 app.use('/api/fees', require('./routes/api/fees'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
